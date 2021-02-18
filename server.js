a3b8ff6d9c01ebbfa53d100bfde006e5f639e173
rhld16 <24809571+rhld16@users.noreply.github.com>
2021-02-18 16:48:04 +0000

    bootstrap + backend changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require('fs');
+const db = require('quick.db');
 var app = express();
 var privateKey = fs.readFileSync('keys/privkey.pem');
 var certificate = fs.readFileSync('keys/fullchain.pem');
@@ -10,14 +11,13 @@ const io = require("socket.io")(https);
 app.use(express.static("views"));
 var peers = [];
 io.on("connect", (socket) => {
-  console.log("A client is connected --", socket.id);
-  peers.push({socket: socket});
-  socket.on('list', function() {
-		let ids = [];
-		for (let peer of peers) {ids.push(peer.socket.id)};
-		socket.emit('listresults', ids);			
-	});
-  socket.on('signal', (to, from, data) => {
+	console.log(new Date().toUTCString()+" Client has connected -- "+socket.id);
+	peers.push({socket: socket});
+	socket.emit("chats", db.get("chats"));
+	let ids = [];
+	for (let peer of peers) {ids.push(peer.socket.id)};
+	socket.emit('list', ids);
+	socket.on('signal', (to, from, data) => {
 		let found = false;
 		for (let peer of peers) {
 			if (peer.socket.id == to) {
@@ -27,12 +27,10 @@ io.on("connect", (socket) => {
 				break;
 			}				
 		}	
-		if (!found) {
-			console.log("Never found peer");
-		}
+		if (!found) console.log("Never found peer");
 	});
-  socket.on('disconnect', function() {
-		console.log("Client has disconnected", socket.id);
+	socket.on('disconnect', function() {
+		console.log(new Date().toUTCString()+" Client has disconnected -- "+socket.id);
 		io.emit('peer_disconnect', socket.id);
 		for (let [i,peer] of peers.entries()) {
 			if (peer.socket.id == socket.id) {
@@ -41,7 +39,10 @@ io.on("connect", (socket) => {
 			}
 		}			
 	});
-  socket.on("chat", (data) => io.emit("chat", data));
+	socket.on("chat", (data) => {
+		db.push('chats', data)
+		io.emit("chat", data)
+	});
 });
 http.listen(80);
 https.listen(443);
@@ -49,20 +50,24 @@ console.log("Server is on!");
 const readline = require("readline");
 const rl = readline.createInterface({input: process.stdin, output: process.stdout});
 
-function bingoNum() {
-  rl.question("Command (num/video/bingo/bingonums/resetbingo): ", function (cmd) {
-    if (cmd === "video") {
-      rl.question("Video URL: ", function (url) {
-        io.sockets.emit("video", url);
-        bingoNum();
-      });
-    } else {
-      bingoNum();
-    }
-  });
+function commands() {
+	rl.question("Command (video/chats/resetchats): ", function (cmd) {
+		if (cmd === "video") {
+		rl.question("Video URL: ", function (url) {
+			io.sockets.emit("video", url);
+			commands();
+		});
+		} else if (cmd === "chats") {
+			console.log(db.get('chats'));
+			console();
+		} else if (cmd === "resetchats") {
+			db.set('chats', []);
+			commands();
+		} else commands();
+	});
 }
 rl.on("close", function () {
   console.log("\nBYE!");
   process.exit(0);
 });
-bingoNum();
+commands();
